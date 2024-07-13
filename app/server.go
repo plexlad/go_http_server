8b2b222fddf4ddcb17efe329b9896478400aa6df
plexlad <brock@tylerjsmith.me>
2024-07-12 22:22:27 -0600

    Read HTTP data

--- app/server.go
+++ app/server.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"net"
+	"net/http"
 	"os"
+	"strings"
 )
 
-func httpResponse(version string, statusCode int, optionalMessage string) []byte {
+func httpResponse(statusCode int, optionalMessage string) []byte {
   // TODO: Proper response HERE
-  responseString := fmt.Sprintf("HTTP/%s %d %s\r\n\r\n", version, statusCode, optionalMessage)
+  responseString := fmt.Sprintf("HTTP/1.1 %d %s\r\n\r\n", statusCode, optionalMessage)
   return []byte(responseString)
 }
 
@@ -24,15 +25,29 @@ func main() {
 
   fmt.Println("Listening...")
 	
-  conn, err := l.Accept()
+  connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+  defer connection.Close() // Adds the connection close to the stack
+  defer fmt.Println("Closing connection")
+  defer l.Close() // Closes the listener after stack is finished
+  defer fmt.Println("Closing server")
   
   fmt.Println("Accepted")
+  
+  requestBuffer := make([]byte, 4096) // big buffer
 
-  conn.Write(httpResponse("1.1", http.StatusOK, "OK"))
+  connection.Read(requestBuffer)
+  if strings.HasPrefix(string(requestBuffer), "GET / HTTP/1.1") {
+    connection.Write(httpResponse(http.StatusOK, "OK"))
+    fmt.Println("Correct link")
+  } else {
+    connection.Write(httpResponse(http.StatusNotFound, "Not found"))
+    fmt.Println("Incorrect link")
+  }
 
   fmt.Println("Responded")
 }
