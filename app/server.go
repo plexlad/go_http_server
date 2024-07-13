f8d20da00335852cffc74b3515c5064337b44f40
plexlad <brock@tylerjsmith.me>
2024-07-12 20:07:38 -0600

    stage 2 complete with function!

--- app/server.go
+++ app/server.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
+	"net/http"
 	"net"
 	"os"
 )
 
+func httpResponse(version string, statusCode int, optionalMessage string) []byte {
+  // TODO: Proper response HERE
+  responseString := fmt.Sprintf("HTTP/%s %d %s\r\n\r\n", version, statusCode, optionalMessage)
+  return []byte(responseString)
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
+  fmt.Println("Listening...")
 	
-	_, err = l.Accept()
+  conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+  
+  fmt.Println("Accepted")
+
+  conn.Write(httpResponse("1.1", http.StatusOK, "OK"))
+
+  fmt.Println("Responded")
 }
